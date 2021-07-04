b579ad0604ad27ddc526458cfb194eb7994fd0cf
song-giung <rldnddl87@gmail.com>
2021-07-04 23:00:49 +0900

    render template file when server initialized

--- wiki.go
+++ wiki.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 )
 
+//global variable
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 type Page struct {
 	Title string
 	Body []byte
@@ -52,12 +54,7 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	err = t.Execute(w, p)
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
